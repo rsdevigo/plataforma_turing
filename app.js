27100e1528614db9c1c4510605ab2f973852b2c4
Rodrigo Devigo <rodrigo.devigo@ifms.edu.br>
2018-11-08 10:48:55 -0300

    reiniciando processo

--- app.js
+++
@@ -1,5 +0,0 @@
-var app = require('./config/server');//importando em uma variavel as informações do server
-
-app.listen(3000, function(){
-	console.log('LIGOU');//mensagem de resposta caso ligue
-});
--- app/routes/admin.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(app){
-	app.get('/formulario_cadastro',function(req,res) { 
-		app.app.controllers.admin.formulario_cadastro(app,req,res);
-	});
-
-	app.post('/cadastros/salvar', function(req,res){
-		app.app.controllers.admin.cadastros_salvar(app,req,res);
-	});
-}
